471786fa974f7c4d1e4095992c120d1c71657d6b
Sarkan <sarkan@ag5.com>
2023-06-15 14:33:32 +0200

    time duration type supported

--- feeder.go
+++ feeder.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"time"
 )
 
 // fieldFeeder this function responsible to put a string value to requested field
 func fieldFeeder(field reflect.Value, value string) error {
 	if !field.CanSet() {
 		return fmt.Errorf("can not set value for field")
+	}
+
+	if reflect.DeepEqual(field.Kind(), reflect.ValueOf(time.Duration(0)).Kind()) {
+		// field is a time duration
+		// check if value all in number make convert it to second
+		intValue, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			// parse string value to integer -> 10s = 10000000000
+			duration, err := time.ParseDuration(value)
+			if err != nil {
+				return fmt.Errorf("failed to parse %s to time.Duration, err: %w", value, err)
+			}
+			intValue = duration.Nanoseconds()
+		}
+
+		field.SetInt(intValue)
+		return nil
 	}
 
 	switch field.Kind() {
